b73a7ad3821678c43b12cd2977a88d42a22f6c6a
kaushalxluciferr <22054382@kiit.ac.in>
2025-08-16 21:13:54 +0530

    ui adjustment-2

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -32,7 +32,7 @@ function Footer() {
         <div>
           <p className='text-base sm:text-xl font-medium mb-3 sm:mb-5'>Contact</p>
           <ul className='flex flex-row flex-wrap gap-x-4 gap-y-2 sm:flex-col sm:gap-1 text-gray-600'>
-            <li className='whitespace-nowrap'>+1212345678</li>
+            <li className='whitespace-nowrap'>+91 89205 41284</li>
             <li className='whitespace-nowrap'>getdetail@email.com</li>
           </ul>
         </div>
